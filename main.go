f740eca9900a04721e809c0971673afd09d9ebcd
Joni Kämppä <joni.kamppa@sc5.io>
2020-08-15 23:00:47 +0200

    Move constants to trading-package

--- main.go
+++ main.go
@@ -2,17 +2,6 @@ package main
 
 import "fmt"
 
-var isNextOperationBuy = true
-var lastOpPrice = 100.00
-
-// BUY Tresholds - Bot State is SELL
-const BULLISH_TRESHOLD = 1.3
-const BEARISH_TRESHOLD = -2.0
-
-// SELL Tresholds - Bot State is BUY
-const PROFIT_TRESHOLD = 0.7
-const STOP_LOSS_TRESHOLD = -0.5
-
 func main() {
 	fmt.Println("Starting the bot-service...")
 
