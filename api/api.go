2097f52f6e062d4d8e911446608bd770d77ad38d
Joni Kämppä <joni.kamppa@sc5.io>
2020-08-17 21:42:36 +0200

    Add dummy API-call functions

---
+++ api/api.go
@@ -0,0 +1,23 @@
+package api
+
+func GetBalance() (currentBalance int) {
+	currentBalance = 100
+
+	return
+}
+
+func GetMarketPrice() {
+	return
+}
+
+func PlaceSellOrder(lastOpPrice *float32) (tradeSuccessful bool) {
+	return true
+}
+
+func PlaceBuyOrder(lastOpPrice *float32) (tradeSuccessful bool) {
+	return true
+}
+
+func GetOperationDetails(operationId string) {
+	return
+}
